856f906a2e234c41b911414e9af9a26610770d75
PlayerThird030 <verevka.rud@gmail.com>
2023-01-10 20:09:30 +0300

    Сделал эксперементальную папку для проверки работоспособности методов + Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Standart;
+
+int DayOfWeek = Checker.CheckInput<int>(x => x < 1 || x > 7, "Введите день недели 1-7:", "Вы вышли ЗА неделю!");// проверка дня недели с условием x < 1 || x > 7
+int Number = Checker.CheckInput<int>(_=> false, "К примеру", string.Empty);//string.Empty = пустые кавычки
+double Booble = Checker.CheckInput<double>(x => x < 1.2 || x > 2.5, "Проверка дабл", "Вы вышли за предел");//string.Empty = пустые кавычки
